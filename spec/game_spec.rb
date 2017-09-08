f9ab1a0cd4976f9d6fd9dcbee74c57d5dd78b39b
k2works <kakimomokuri@gmail.com>
2017-09-08 10:53:35 +0900

    テストケース新規作成

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe BowlingGameRuby::Game do
 
   describe '#score_for_frame' do
     context 'when four throws' do
-      it 'is scored' do
+      it 'scored' do
         g = Game.new
         g.add(5)
         g.add(4)
@@ -33,6 +33,12 @@ RSpec.describe BowlingGameRuby::Game do
         g.add(2)
         expect(g.score_for_frame(1)).to eq(9)
         expect(g.score_for_frame(2)).to eq(18)
+      end
+    end
+
+    context 'when spare' do
+      it 'scored' do
+        g = Game.new
       end
     end
   end
