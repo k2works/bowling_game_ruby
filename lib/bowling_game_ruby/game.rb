4f84a524baa9dc2db3f86e837709de4d5befb1b1
k2works <kakimomokuri@gmail.com>
2017-09-08 10:45:05 +0900

    フレーム別スコア計算メソッド新規作成
    フレーム別にスコアを集計できるようにする。
    １ゲーム１０フレームで１フレームあたり２回ボールを投げれる。
    ただし、１０フレームは最大３回ボールを投げれるので１ゲーム
    に投げられるボールの数は２１とする。

--- lib/bowling_game_ruby/game.rb
+++ lib/bowling_game_ruby/game.rb
@@ -6,14 +6,26 @@ module BowlingGameRuby
 
     def initialize
       @score = 0
+      @current_throw = 0
+      @throws = Array.new(21, 0)
     end
 
     def add(pins)
+      @throws[@current_throw] = pins
+      @current_throw += 1
       @score += pins
     end
 
     def score_for_frame(the_frame)
-      0
+      score = 0
+      ball = 0
+      current_frame = 0
+      while current_frame < the_frame
+        score += @throws[ball] + @throws[ball + 1]
+        ball += 2
+        current_frame += 1
+      end
+      score
     end
   end
 end
