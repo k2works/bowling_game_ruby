2eb25ad118245037cb957cbb15f391dd88a33618
k2works <kakimomokuri@gmail.com>
2017-09-12 11:00:35 +0900

    フレーム別スコア計算メソッド更新
    ストライクの場合の条件分岐を追加。
    第１投が１０の場合ストライクとなり第２・３投の合計に１０を合計した値がフレームのスコアとなる。
    第１投がストライクの場合次のフレームの値を更新する。

--- lib/bowling_game_ruby/game.rb
+++ lib/bowling_game_ruby/game.rb
@@ -13,7 +13,7 @@ module BowlingGameRuby
     end
 
     def score
-      score_for_frame(get_current_frame-1)
+      score_for_frame(get_current_frame - 1)
     end
 
     def add(pins)
@@ -30,17 +30,26 @@ module BowlingGameRuby
       while current_frame < the_frame
         first_throw = @throws[ball]
         ball += 1
-        second_throw = @throws[ball]
-        ball += 1
-        frame_score = first_throw + second_throw
 
-        # スペアの得点計算には次のフレームの第１投が必要
-        score += if frame_score == 10
-                   frame_score + @throws[ball]
-                 else
-                   frame_score
-                 end
-        current_frame += 1
+        if first_throw == 10
+          second_throw = @throws[ball]
+          third_throw = @throws[ball + 1]
+          frame_score = first_throw + second_throw + third_throw
+          score = frame_score
+          current_frame += 1
+        else
+          second_throw = @throws[ball]
+          ball += 1
+          frame_score = first_throw + second_throw
+
+          # スペアの得点計算には次のフレームの第１投が必要
+          score += if frame_score == 10
+                     frame_score + @throws[ball]
+                   else
+                     frame_score
+                   end
+          current_frame += 1
+        end
       end
       score
     end
@@ -53,7 +62,12 @@ module BowlingGameRuby
 
     def addjust_current_frame
       if @first_throw
-        @first_throw = false
+        if @throws[@current_throw - 1] == 10
+          @current_frame += 1
+          @first_throw = true
+        else
+          @first_throw = false
+        end
       else
         @current_frame += 1
         @first_throw = true
